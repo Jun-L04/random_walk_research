505d8a05392a588ab0cfa4a3262cecb24e24a1c0
Jun-L04 <LuJunyang04@gmail.com>
2023-08-20 19:55:02 -0400

    reorganized file paths

--- walk.py
+++ walk.py
@@ -6,6 +6,7 @@ from scipy.stats import entropy
 
 # have to run in "interactive window"
 
+
 def create_graphs(n_cycle, n_grid):
     # cycle graph: nodes are arranged in a circle
     Gc = nx.cycle_graph(n_cycle)
@@ -19,20 +20,22 @@ def create_graphs(n_cycle, n_grid):
     # the graphs are not randomly generated
     # but is fine since the walk is random
 
+
 # create cycle and grid graphs with 5 and 3 nodes respectively
-# adjacency matrices 
+# adjacency matrices
 Ac, Ag = create_graphs(15, 15)
 
-# print them 
+# print them
 print("Cycle graph")
 print(Ac)
 print("Grid graph")
 print(Ag)
 
 # calculate A^k where k is steps of random walk
-# the resulting matrix's entries are 
-# the number of different walks of 
+# the resulting matrix's entries are
+# the number of different walks of
 # length k, between the two nodes
+
 
 def compute_matrices_and_entropy(Ac, Ag, k_max=1000):
     # compute Ac^k and Ag^k for each k, and their entropies
@@ -46,8 +49,8 @@ def compute_matrices_and_entropy(Ac, Ag, k_max=1000):
         Agk = np.linalg.matrix_power(Ag, k)
 
         # compute random VxW matrices Mck and Mgk
-        # with row/columne dimensions derived from .shape 
-        # but why? 
+        # with row/columne dimensions derived from .shape
+        # but why?
         Mck = np.random.rand(Vc, Wc)
         Mgk = np.random.rand(Vg, Wg)
 
@@ -65,7 +68,7 @@ def compute_matrices_and_entropy(Ac, Ag, k_max=1000):
     return entropy_c, entropy_g
 
 
-# higher the entropy the harder it is 
+# higher the entropy the harder it is
 # to perdict the movement of the random walker
 # grid graph seems to have a higher entropy than cycle graph
 
@@ -75,11 +78,13 @@ entropy_c, entropy_g = compute_matrices_and_entropy(Ac, Ag)
 print("Entropy of cycle graph is: ", entropy_c)
 print("Entropy of grid graph is: ", entropy_g)
 
-# method to plot everything and compare the entropies 
+# method to plot everything and compare the entropies
+
+
 def plot_entropy(entropy_c, entropy_g):
-    
+
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
+
     plt.figure(figsize=(10, 6))
     plt.plot(entropy_c, label="Cycle graph")
     plt.plot(entropy_g, label="Grid graph")
@@ -91,8 +96,9 @@ def plot_entropy(entropy_c, entropy_g):
     plt.grid(True)
     plt.show()
 
+
 # plot the entropy values
 plot_entropy(entropy_c, entropy_g)
 
 # plots how entropy changes as number of steps change
-# all seem to increase logarithmically? 
+# all seem to increase logarithmically?
